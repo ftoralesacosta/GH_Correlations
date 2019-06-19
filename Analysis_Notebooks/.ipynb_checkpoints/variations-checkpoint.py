45ec85050c89ee47c8a5ca3b7c07a6540111ef88
Fernando Torales - Acosta <fernando@cori08.nersc.gov>
2019-06-19 14:54:07 -0700

    Plot edits, 16dphi

---
+++ Analysis_Notebooks/.ipynb_checkpoints/variations-checkpoint.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+from ipywidgets import interact, interactive, fixed, interact_manual
+import ipywidgets as widgets
+
+description_string = ""
+
+def Get_Variation(string):
+    return destring
+
+all_strings = ["05zT","05zT_working_old","05zT_3bins","05zT_2bins"," ","1zT","15zT","2zT"," ","pT_Rebin_3","pT_Rebin_5"," ","zT_Rebin_5","zT_Rebin_6","zT_Rebin_8","zT_Rebin_9"]
+
+def f(Variation):
+    return Variation
+def Do_Variation(description_string):
+    string = interact(f, Variation=all_strings);
+    description_string.join(string)
