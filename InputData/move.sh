96cb480a93df0d2c0f7a68c7d79b99cecf5a3921
Fernando Torales - Acosta <fernando@cori08.nersc.gov>
2019-07-16 13:27:21 -0700

    zT_Rebin_8_006zT06zTOldBinNewNewPurity

--- InputData/move.sh
+++ InputData/move.sh
@@ -3,3 +3,8 @@ mv *GeVTracks_Mixed_300_Correlation_Lambda.root $1
 mv *_SE_L0_Correlation.root $1
 cd ../Event_Mixing/
 ./divide_mix.sh ../InputData/$1/
+
+git pull
+git add -f $1
+git commit -m "${1}"
+git push
