f6ae46e6a397febe7ef0e1e7e49e9622ae75e9ea
Fernando Torales - Acosta <fernando@cori08.nersc.gov>
2019-08-05 20:44:08 -0700

    pp Implementation

--- InputData/move.sh
+++ InputData/move.sh
@@ -3,8 +3,12 @@ mv *GeVTracks_Mixed_300_Correlation_Lambda.root $1
 mv *_SE_L0_Correlation.root $1
 cd ../Event_Mixing/
 ./divide_mix.sh ../InputData/$1/
+mkdir ../Analysis_Notebooks/pics/LO/$1/
+touch ../Analysis_Notebooks/pics/LO/$1/.gitignore
 
 git pull
-git add -f $1
+git add -f ../InputData/$1
+git add ../Corr_config.yaml
+git add ../Analysis_Notebooks/pics/LO/$1/
 git commit -m "${1}"
 git push
