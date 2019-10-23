21316c56e82cd39b1339eaede22cc4b700889abf
Fernando Torales Acosta <fernando_tta@berkeley.edu>
2019-10-22 18:31:08 -0700

    local notebook updates

--- Analysis_Notebooks/.ipynb_checkpoints/default_values-checkpoint.py
+++ Analysis_Notebooks/.ipynb_checkpoints/default_values-checkpoint.py
@@ -158,7 +158,7 @@ description_string = default_string
 #description_string = "zT_Rebin_8_006zT06zT13fnewONLY"
 #description_string = "zT_Rebin_8_006zT06zT13fnewChi2"
 #description_string = "zT_Rebin_8_006zT06zT13fnewChi36noWeights"
-description_string = "zT_Rebin_8_006zT06zT13fnewChi2noWeights"
+#description_string = "zT_Rebin_8_006zT06zT13fnewChi2noWeights"
 
  
 #description_string = "zT_Rebin_7_006zT06zT"
